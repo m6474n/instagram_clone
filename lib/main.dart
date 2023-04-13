88b6196901b2a5a93b07c97084132515fa8ce76d
Muhammad Mohsin <m.mohsin2055@gmail.com>
2023-04-14 01:13:36 +0500

    database integerated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobileScreenLayout.dart';
 import 'package:instagram_clone/responsive/responsive.dart';
 import 'package:instagram_clone/responsive/webScreenLayout.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp;
   runApp(const MyApp());
 }
 
@@ -15,10 +18,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Instagram Clone',
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
-      home: ResponsiveLayout(webScreenLayout: WebScreen(), mobileScreenLayout: MobileScreen(),)   );
+        debugShowCheckedModeBanner: false,
+        title: 'Instagram Clone',
+        theme: ThemeData.dark()
+            .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+        home: ResponsiveLayout(
+          webScreenLayout: WebScreen(),
+          mobileScreenLayout: MobileScreen(),
+        ));
   }
 }
-
